9ff1c7df75faae467d443721fb6c7c34a2f5cee3
MateuGit <facundomateu@hotmail.com>
2021-08-26 18:27:21 -0300

    Fix ms

--- bank/src/main/java/tesis/bank/repositories/CardsRepository.java
+++
@@ -1,9 +0,0 @@
-package tesis.bank.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import tesis.bank.models.Card;
-
-@Repository
-public interface CardsRepository extends JpaRepository<Card, Long> {
-}
